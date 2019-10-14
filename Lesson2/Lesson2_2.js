c6acaa5a8660db924f09cd7454f45fcb2ee07446
atom0511 <totrungatom@gmail.com>
2019-10-14 12:35:29 +0700

    Lesson2

---
+++ Lesson2/Lesson2_2.js
@@ -0,0 +1,30 @@
+let soTien = 123456789;
+let copySoTien = soTien.toString();
+console.log(copySoTien);
+let lengSoTien = 0;
+let charNull = "";
+
+while (copySoTien.charAt(lengSoTien) != charNull) {
+    lengSoTien++;
+}
+
+let count = 0;
+while (copySoTien.charAt(lengSoTien - 1) != charNull) {
+    lengSoTien -= 1;
+    count++;
+    console.log("count = " + count);
+    console.log("leng = " + lengSoTien);
+    if (count == 3 && lengSoTien != 0) {
+        console.log("vao");
+        let a = lengSoTien;
+        copySoTien = copySoTien.replace(
+            copySoTien.charAt(lengSoTien),
+            "," + copySoTien.charAt(lengSoTien)
+        );
+        count = 0;
+    }
+    if (lengSoTien == 0) {
+        break;
+    }
+}
+console.log(copySoTien);
