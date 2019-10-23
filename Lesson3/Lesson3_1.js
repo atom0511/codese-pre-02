7b4bbe3c164eecba8f63beb7faa48b3585860b4a
Atom <50996589+atom0511@users.noreply.github.com>
2019-10-23 11:44:59 +0700

    Update Lesson3_1.js

--- Lesson3/Lesson3_1.js
+++ Lesson3/Lesson3_1.js
@@ -1,10 +1,13 @@
-let loiBH = "Mrs          Dau and the order of the Phoenix";
+let loiBH = "Mrs       Dau and the order of the Phoenix";
+
+console.log(loiBH);
 for (let i = 1; i < loiBH.length; i++) {
-    if (loiBH[i] === loiBH[i - 1] && loiBH[i] === 0) {
-        loiBH[i] = null;
+    if (loiBH[i] === loiBH[i - 1] && loiBH[i] === " ") {
+        loiBH = loiBH.replace(loiBH[i], "");
+        i--;
     }
 }
-console.log(loiBH);
+
 let copy = loiBH.split(" ");
 for (let i = 0; i < copy.length; i++) {
     setTimeout(function() {
